404faed65fd578636b96ec7c6e0a5b15c9c2db70
Benjamin Lim <blym@nus.edu.sg>
2014-05-28 14:55:34 +0800

    renamed point variable in Location model to geopt, updated all other files accordingly.

--- maphack.py
+++ maphack.py
@@ -25,7 +25,7 @@ class Location(ndb.Model):
 	location_id = ndb.IntegerProperty()
 	name = ndb.StringProperty()
 	address = ndb.StringProperty()
-	point = ndb.GeoPtProperty()
+	geopt = ndb.GeoPtProperty()
 
 class Inventory(ndb.Model):
 	# Key: user id
@@ -204,7 +204,7 @@ class LocationsAddLocation(webapp2.RequestHandler):
 			location.address = self.request.get('address')
 			location.latitude = self.request.get('latitude')
 			location.longitude = self.request.get('longitude')
-			location.point = ndb.GeoPt(self.request.get('latitude'), self.request.get('longitude'))
+			location.geopt = ndb.GeoPt(self.request.get('latitude'), self.request.get('longitude'))
 			user.next_location += 1
 
 			user.put()
